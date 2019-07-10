85035ecc474e1ac60e278d9386454ab5c3bc4a48
Benoit DAUBA <contact@dauba.net>
2019-07-10 23:33:40 +0200

    ADD GITHUB link dans le footer

--- template/footer.php
+++ template/footer.php
@@ -2,5 +2,7 @@
 <footer class="py-5 bg-dark">
     <div class="container">
         <p class="m-0 text-center text-white">Copyright &copy; <a href="http://benoit.devel.webforce-code.fr">Benoit DAUBA </a> 2019 - WEBFORCE3 Carpentras</p>
+        <p class="m-0 text-center text-white">Retrouvez les sources de <a href="https://github.com/daubanet/bookshop">bookshop </a> sur GITHUB.</p>
+        <p></p>
     </div>
 </footer>
